4f15b6e0777b3e94e8fd8f017eb6c2f88d323888
krisikostadinov <krisi.199898@gmail.com>
2022-03-26 12:30:46 +0200

    Setup React app and created navbar.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='navbar'>
+            <div className="navbar-container">
+                <div className='brand'>
+                    <NavLink to='/'>Movies App</NavLink>
+                </div>
+                <div>
+                    <form>
+                        <input type='text' name='search' placeholder='Search Movies...' />
+                        <button>
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+                <ul>
+                    <li>
+                        <NavLink to='/users/login'>Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/users/register'>Register</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
